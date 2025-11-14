d8dc109ea1fe2a0c931da38da848971594bb6f26
gilgameshone <91382279+gilgameshone@users.noreply.github.com>
2025-11-14 11:23:38 +0900

    try EEHANDS

--- keyboards/crkbd/keymaps/gilgamesh/config.h
+++ keyboards/crkbd/keymaps/gilgamesh/config.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_MATRIX_I2C
 
 /* Select hand configuration */
+#define EE_HANDS
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
